94b4ebcfa3e26e57ddf33c2d2a2a1e2db6b36b03
Raymon Smalley <raysmalley@hotmail.com>
2022-11-10 16:59:32 -0500

    Update new-pc-setup.ps1

--- new-pc-setup.ps1
+++ new-pc-setup.ps1
@@ -1,7 +1,7 @@
 ﻿# Parameters for excluding app installs (broken atm...)
 #param($Exclude)
 
-$LastUpdated = '11/07/2022  '
+$LastUpdated = '11/10/2022  '
 
 # Set window title
 $host.UI.RawUI.WindowTitle = "New PC Setup Script - $env:COMPUTERNAME"
